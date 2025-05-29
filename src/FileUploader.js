30860e763d656bd430f35cf7239a100a8f1c0b29
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-05-30 01:20:55 +0530

    changed the default

---
+++ src/FileUploader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FileUploader(props) {
+    
+
+    return (
+        <>
+           <h1>File uploader</h1> 
+        </>
+    )
+}
+
+
+export default FileUploader
