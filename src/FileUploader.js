e103495e87d734dfb1d91ec8f65bf1268f5882a9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-05-30 01:29:04 +0530

    initial setup of the ui

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 function FileUploader(props) {
-    
+  const [files, setFiles] = useState([]);
+  const fileInputRef = useRef(null);
 
-    return (
-        <>
-           <h1>File uploader</h1> 
-        </>
-    )
+  return (
+    <div>
+      <input
+        type="file"
+        multiple
+        ref={fileInputRef}
+        onChange={handleFileChange}
+      />
+      <button>Browse Files</button>
+      <div>Drop files here</div>
+    </div>
+  );
 }
 
-
-export default FileUploader
+export default FileUploader;
