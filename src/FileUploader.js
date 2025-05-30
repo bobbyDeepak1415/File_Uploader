ec2e233fcaa554096a6cd5b9437a9ab0d47832e1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-05-30 16:38:59 +0530

    added functionality

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -4,16 +4,57 @@ function FileUploader(props) {
   const [files, setFiles] = useState([]);
   const fileInputRef = useRef(null);
 
+  const handleFileChange = (e) => {
+    const selectedFiles = Array.from(e.target.value);
+    setFiles((prev) => [...prev, ...selectedFiles]);
+  };
+
+  const openFileDialog = () => {
+    fileInputRef.current.click();
+  };
+
+  const preventDefaults = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const droppedFiles = Array.from(e.dataTransfer.files);
+    setFiles((prev) => [...prev, ...droppedFiles]);
+  };
+
   return (
     <div>
       <input
         type="file"
+        hidden
         multiple
         ref={fileInputRef}
         onChange={handleFileChange}
       />
-      <button>Browse Files</button>
-      <div>Drop files here</div>
+      <button onClick={openFileDialog}> Browse Files</button>
+      <div
+        onDrop={handleDrop}
+        onDragOver={preventDefaults}
+        onDragEnter={preventDefaults}
+        style={{
+          border: "2px dashed",
+          padding: "100px",
+          maggin: "10px",
+          textAlign: "center",
+        }}
+      >
+        Drop files here
+      </div>
+      <div>
+        <h4>Uploaded files here</h4>
+        <ul>
+          {files.map((item, index) => {
+            return <li key={index}>{item.name}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
