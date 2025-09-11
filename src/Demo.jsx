ca1d162cbed43ddb958d6b5bea8630ec86d02af1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-11 18:11:17 +0530

    added the ui part

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function Demo(props) {
+  const inputFileRef = useRef(0);
+  const [files, setFiles] = useState([]);
+
+  const handleSelect = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+    setFiles([...files, ...selectedFiles]);
+
+  };
+  const handleClick = () => {
+    // inputFileRef.value=""
+    inputFileRef.current.click()
+  };
+
   return (
     <div>
-        <input onChange={openExplorer} hidden></input>
-        <button>Upload Files</button>
+      <h2>FileUploader</h2>
+      <input type="file" ref={inputFileRef} multiple onChange={handleSelect} hidden></input>
+      <button onClick={handleClick}>Upload Files</button>
     </div>
   );
 }
