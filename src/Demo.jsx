b9a62c8b38575ee26b6e1554effe7f62118cc1d0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-11 23:12:17 +0530

    practiced in demo.jsx

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -7,18 +7,51 @@ export default function Demo(props) {
   const handleSelect = (e) => {
     const selectedFiles = Array.from(e.target.files);
     setFiles([...files, ...selectedFiles]);
-
   };
   const handleClick = () => {
-    // inputFileRef.value=""
-    inputFileRef.current.click()
+    inputFileRef.value=""
+    inputFileRef.current.click();
+  };
+
+  const handleDefaults = (e) => {
+    e.stopPropogation()
+    e.preventDefault();
+  };
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const droppedFiles = Array.from(e.dataTransfer.files);
+    setFiles([...files, ...droppedFiles]);
   };
 
   return (
     <div>
       <h2>FileUploader</h2>
-      <input type="file" ref={inputFileRef} multiple onChange={handleSelect} hidden></input>
+      <input
+        type="file"
+        ref={inputFileRef}
+        multiple
+        onChange={handleSelect}
+        hidden
+      ></input>
       <button onClick={handleClick}>Upload Files</button>
+      <div
+        onDragEnter={handleDefaults}
+        onDragOver={handleDefaults}
+        onDrop={handleDrop}
+        style={{
+          border: "1px dashed blue",
+          height: "400px",
+          marginTop: "10px",
+        }}
+      >
+        <p>Drop your files here</p>
+      </div>
+      <div>
+        <h2>Your files here:</h2>
+        {files.map((item, index) => {
+          return <li key={index}>{item.name}</li>;
+        })}
+      </div>
     </div>
   );
 }
