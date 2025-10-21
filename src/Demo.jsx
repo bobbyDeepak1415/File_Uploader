f49586f17b8ab23731e7d1ba3c6063d4c2e2e585
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-21 23:06:06 +0530

    strings ques 8

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,57 +1,21 @@
-import React, { useRef, useState } from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Demo(props) {
-  const inputFileRef = useRef(0);
-  const [files, setFiles] = useState([]);
+class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {color:props.color};
+    }
 
-  const handleSelect = (e) => {
-    const selectedFiles = Array.from(e.target.files);
-    setFiles([...files, ...selectedFiles]);
-  };
-  const handleClick = () => {
-    inputFileRef.value=""
-    inputFileRef.current.click();
-  };
-
-  const handleDefaults = (e) => {
-    e.stopPropogation()
-    e.preventDefault();
-  };
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const droppedFiles = Array.from(e.dataTransfer.files);
-    setFiles([...files, ...droppedFiles]);
-  };
-
-  return (
-    <div>
-      <h2>FileUploader</h2>
-      <input
-        type="file"
-        ref={inputFileRef}
-        multiple
-        onChange={handleSelect}
-        hidden
-      ></input>
-      <button onClick={handleClick}>Upload Files</button>
-      <div
-        onDragEnter={handleDefaults}
-        onDragOver={handleDefaults}
-        onDrop={handleDrop}
-        style={{
-          border: "1px dashed blue",
-          height: "400px",
-          marginTop: "10px",
-        }}
-      >
-        <p>Drop your files here</p>
-      </div>
-      <div>
-        <h2>Your files here:</h2>
-        {files.map((item, index) => {
-          return <li key={index}>{item.name}</li>;
-        })}
-      </div>
-    </div>
-  );
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
+
+Demo.propTypes = {};
+
+export default Demo;
