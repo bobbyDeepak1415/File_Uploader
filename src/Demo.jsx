27a2519f84716d2a78d2c155521fc0ffaf358d78
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-06 16:41:43 +0530

    setting up

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 function Demo() {
+  const [users, setUsers] = useState([]);
+  const inputFileRef = useRef(null);
+
+  const handleClick = () => {
+    inputFileRef.current.click();
+    inputFileRef.current.value = "";
+  };
+
+  const handleSelect = () => {};
+
   return (
     <div>
       <h2>Demo</h2>
+      <input
+        ref={inputFileRef}
+        type="file"
+        multiple
+        hidden
+        onChange={handleSelect}
+      ></input>
+      <button style={{marginBottom:"2rem"}} onClick={handleClick}>Click to browse</button>
+      <div
+        style={{
+          border: "1px dashed",
+          height: "40vh",
+          width: "80vw",
+          margin:"auto"
+        }}
+      >
+        Drop your files here:
+      </div>
     </div>
   );
 }
