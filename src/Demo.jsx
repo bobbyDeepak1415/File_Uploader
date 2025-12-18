6cef88173d7c3b5bbe56a56ce656c3adf2415e36
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-19 00:03:41 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,23 +1,7 @@
 import React, { useState } from "react";
 
 const Demo = () => {
-
-  // const [count,setCount]=useState(0)
-
-  let count=0
-
-const increment=()=>{
-count=count+1
-}
-
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={()=>increment}>Click</button>
-      {/* <button onClick={()=>setCount(count+1)}>Click</button> */}
-    </div>
-  );
+  return <></>;
 };
 
 export default Demo;
-,
