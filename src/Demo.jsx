6c0049916ac6595237d0d8f9b2cb2105ed3d6f78
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-19 23:40:09 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 
 const Demo = () => {
 
-  return <>
-  
-  </>
+  const [input,setInput]=useState("")
+  const [items,setItems]=useState([])
+
+  return (
+    <div>
+      <input></input>
+      <button>Add</button>
+      <div>
+        {items.map((item,index)=>{
+          return <li key={index}>{item}</li>
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
-
-
-.
