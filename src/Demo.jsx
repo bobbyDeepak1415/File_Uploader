8cf10c5a604ee44f397708faf4606b654596b612
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-19 23:56:52 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 
 const Demo = () => {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
 
-  const [input,setInput]=useState("")
-  const [items,setItems]=useState([])
-
-  const handleClick=()=>{
-    
-  }
+  const handleClick = (e) => {
+    e.preventDefault();
+    setItems([...items, input]);
+    setInput("");
+  };
 
   return (
     <div>
-      <input value={input} onChange={(e)=>setInput(e.target.value)}></input>
+      <input value={input} onChange={(e) => setInput(e.target.value)}></input>
       <button onClick={handleClick}>Add</button>
-      <div>
-        {items.map((item,index)=>{
-          return <li key={index}>{item}</li>
-        })}
+      <div style={{ display: "flex" }}>
+        <ul>
+          {items.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
       </div>
     </div>
   );
