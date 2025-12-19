9061c7a9a7e3ab2ef841c55cef9a6bd88468f50c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-19 23:41:21 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -5,10 +5,14 @@ const Demo = () => {
   const [input,setInput]=useState("")
   const [items,setItems]=useState([])
 
+  const handleClick=()=>{
+    
+  }
+
   return (
     <div>
-      <input></input>
-      <button>Add</button>
+      <input value={input} onChange={(e)=>setInput(e.target.value)}></input>
+      <button onClick={handleClick}>Add</button>
       <div>
         {items.map((item,index)=>{
           return <li key={index}>{item}</li>
