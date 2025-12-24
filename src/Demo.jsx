a258782cb9e27a8fdc2e30e07de13cd85083cedb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-24 15:26:39 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Demo = () => {
-  return <>
-  <h1>Helloj</h1>
-  </>;
+
+  const [products,setProducts]=useState([])
+
+  const fetchData=async()=>{
+
+    try{
+
+      const data = await axios.get("https://fakestoreapi.com/products");
+      setProducts(data.data.products)
+    }catch(e){
+      console.error("failed fetching...",e)
+    }
+
+
+  }
+
+  return <div>
+  
+  {}
+  
+
+  </div>;
 };
 
 export default Demo;
