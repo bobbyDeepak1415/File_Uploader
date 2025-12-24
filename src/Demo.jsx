85626e87d1597afcf93263f21e1d13497193a469
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-24 15:24:29 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,41 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-
 const Demo = () => {
-  const [products, setProducts] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("https://dummyjson.com/products");
-
-      setProducts(response.data.products);
-    } catch (e) {
-      console.error("failed fetching", e);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const styles = {
-    main: {
-      padding: "20px",
-    },
-    title: {
-      color: "#5C6AC4",
-    },
-  };
-
-  return (
-    <div style={styles.main}>
-      <div>
-        {products.map((product, id) => {
-          return <li key={product.id}>{product.title}</li>;
-        })}
-      </div>
-    </div>
-  );
+  return <>
+  <h1>Helloj</h1>
+  </>;
 };
 
 export default Demo;
