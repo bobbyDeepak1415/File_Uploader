8553e1d43612c74fa27c2f77deb373e180ed9b88
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-24 15:31:00 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -6,22 +6,26 @@ const Demo = () => {
 
   const fetchData = async () => {
     try {
-      const data = await axios.get("https://fakestoreapi.com/products");
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/comments"
+      );
       setProducts(data.data);
     } catch (e) {
       console.error("failed fetching...", e);
     }
   };
 
-  useEffect(()=>{
-fetchData()
-  },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  return <div>{
-    products.map((product,id)=>{
-      return <li key={id}>{product.title}</li>
-    })
-    }</div>;
+  return (
+    <div>
+      {products.map((product, id) => {
+        return <li key={id}>{product.name}</li>;
+      })}
+    </div>
+  );
 };
 
 export default Demo;
