0ac477ed53fe925e9748ffeba3ee2e1f28bb5095
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-06 22:41:52 +0530

    debugging

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -6,20 +6,3 @@
 
 // export default Demo;
 
-// let arr1 = student.forEach((student) => {
-//   if (student.rollNo === rollNo) {
-//     student.name = name;
-//   }
-// });
-
-//let  studentsList = [{ rollNo: 1111, name: 'name1' }, { rollNo: 2222, name: 'name2' }]
-//
-// const [student, setStudent] = useState(studentsList);
-//
-// function updateStudentName(rollNo,name){
-
-
-
-// setStudent(prev=>prev.map(student=>student.rollNo===rollNo? {...student,name}:student))
-// }
-
