9cc4f87e4519c8c01c822a433f88272e7110d4c0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-06 22:07:08 +0530

    debugging

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,8 +1,20 @@
-const Demo = () => {
-  return <h1>
+let studentsList = [
+  { rollNo: 1111, name: "name1" },
+  { rollNo: 2222, name: "name2" },
+];
 
-    
-  </h1>;
+const Demo = (rollNo, name) => {
+  const [student, setStudent] = useState(studentsList);
+
+  let arr1 = student.forEach((student) => {
+    if (student.rollNo === rollNo) {
+      student.name = name;
+    }
+  });
+  setStudent([...arr1]);
 };
+
+
+Demo(1111,"Bobby")
 
 export default Demo;
