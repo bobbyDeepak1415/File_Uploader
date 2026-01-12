2b2a8a7ebe46d60ac46e668303cc2a52d28e448e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-12 18:19:33 +0530

    set-up done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
 
 const useDemo = () => {
+  const [currentStatus, setCurrentStatus] = useState("");
 
-  const [currentStatus,setCurrentStatus]=useState("")
+  const indexRef = useRef(0);
 
-  const statuses=["active","ofline","unKnown","inactive"]
+  const statuses = ["active", "ofline", "unKnown", "inactive"];
 
-  
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      setCurrentStatus(statuses[indexRef.current]);
+      indexRef.current = (indexRef.current + 1) % statuses.length;
+    }, 1200);
+
+    return () => clearInterval(intervalRef);
+  },[]);
 
   return (
     <>
-    <h1>Hello</h1>
-    <h1>Hello</h1>
-    <h1>Hello</h1>
-    <p>{currentStatus}</p>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <p>{currentStatus}</p>
     </>
-  )
-
-
+  );
 };
 
 export default useDemo;
