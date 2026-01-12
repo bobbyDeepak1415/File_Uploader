14b5ee3954d1290b7d8cc51a7432df0e2a4a30fa
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-12 18:15:24 +0530

    set-up done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -2,7 +2,22 @@ import React, { useEffect, useRef, useState } from "react";
 
 const useDemo = () => {
 
+  const [currentStatus,setCurrentStatus]=useState("")
+
+  const statuses=["active","ofline","unKnown","inactive"]
+
   
+
+  return (
+    <>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <p>{currentStatus}</p>
+    </>
+  )
+
+
 };
 
 export default useDemo;
