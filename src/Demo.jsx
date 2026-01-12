ad4eae56d3e4cfad620233eaaf4a92f565e8f736
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-12 18:41:50 +0530

    set-up done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,22 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const useDemo = () => {
-  const [currentStatus, setCurrentStatus] = useState("");
-
-  const statuses = ["offline", "online", "unKnown", "loggedOut"];
-
-  const indexRef = useRef(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentStatus(statuses[indexRef.current]);
-      indexRef.current = (indexRef.current + 1) % statuses.length;
-    }, 1200);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return <p>{currentStatus}</p>;
-};
+const useDemo = () => {};
 
 export default useDemo;
