6953ac971155f9b0c3c9f5918b5b691ef9f12660
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-25 03:30:38 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,24 +1,66 @@
 import { useState } from "react";
 
 const Demo = () => {
+  const [newTask, setNewTask] = useState("");
 
-    const [newTask,setNewTask]=useState("")
+  const [taskStages, setTaskStages] = useState([[], [], [], []]);
 
-    return (
+  const stages = ["Backlogs", "Todo", "Ongoing", "Done"];
 
-        <div className="h-[100vh] bg-gray-400 ">
-            <div>
-                <form>
-                    <input></input>
-                    <button className="bg-teal-300">Add Task</button>
-                </form>
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (!newTask.trim()) return;
+
+    const updatedStages = structuredClone(taskStages);
+
+    const task = { name: newTask.trim(), stage: 0 };
+
+    updatedStages[0].push(task);
+    setTaskStages(updatedStages);
+    setNewTask("");
+  };
+
+  return (
+    <div className="h-[100vh] bg-gray-400 flex flex-col">
+      <div className="p-4 mb-8">
+        <form>
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          ></input>
+          <button className="bg-teal-300" onClick={handleAddTask}>
+            Add Task
+          </button>
+        </form>
+      </div>
+      <div className="flex justify-center">
+        {stages.map((stage, stageIndex) => {
+          return (
+            <div
+              className="h-[70vh] border-2 w-[20vw] m-auto border-black flex flex-col"
+              key={stageIndex}
+            >
+              <h1 className="text-lg">{stage}</h1>
+              <ul>
+                {taskStages.map((task, taskIndex) => {
+                  return (
+                    <li key={taskIndex}>
+                      <span>{task.name}</span>
+                      <span>
+                        <button>⬅️</button>
+                        <button>➡️</button>
+                        <button onClick={}>❌</button>
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-            <div>
-
-            </div>
-        </div>
-
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
