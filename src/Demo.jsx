cc5fa16b96ebc362a617fde48f468b44bca9112f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-25 03:10:41 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,5 +1,24 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
-const useDemo = () => {};
+const Demo = () => {
 
-export default useDemo;
+    const [newTask,setNewTask]=useState("")
+
+    return (
+
+        <div className="h-[100vh] bg-gray-400 ">
+            <div>
+                <form>
+                    <input></input>
+                    <button className="bg-teal-300">Add Task</button>
+                </form>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+    )
+};
+
+export default Demo;
