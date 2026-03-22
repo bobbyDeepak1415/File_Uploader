4100532c301fe2e3040305918fa580dac2cf1851
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-23 01:23:01 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import handleClick from './handleClick'
 
 const Demo = () => {
+
+  const [count,setCount]=useState(0)
+
+
+  const 
+
   return (
     <div>
+
+      <button onClick={handleClick}>click <span>{count}</span></button>
       
     </div>
   )
