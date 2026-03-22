c04772dd359b80bf53f7972341aead599dd7befb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-23 01:43:38 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,28 +1,5 @@
-import React, { useState } from "react";
-
 const Demo = () => {
-  const [count, setCount] = useState([]);
-
-  // [1]
-  // [1,2]
-  // [1,2,3]
-
-  const handleClick = () => {
-    setCount((prev) => [...prev, prev.length + 1]);
-  };
-
-  return (
-    <div>
-      <button onClick={handleClick}>click</button>
-      <div>
-        <ul>
-          {count.map((count) => {
-            return <li>{count}</li>;
-          })}
-        </ul>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Demo;
