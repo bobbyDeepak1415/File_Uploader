81ea2248f5baddc050846191000a0a6e7e2e5312
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-12 19:53:50 +0530

    setup done

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 function FileUploader(props) {
   const [files, setFiles] = useState([]);
   const inputFileRef = useRef(null);
 
-  useEffect(() => {
-    localStorage.setItem("files", JSON.stringify(files));
-  }, [files]);
+  // useEffect(() => {
+  //   localStorage.setItem("files", JSON.stringify(files));
+  // }, [files]);
 
   const handleSelect = (e) => {
     const selectedFiles = Array.from(e.target.files);
@@ -20,6 +20,7 @@ function FileUploader(props) {
 
   const handleClick = () => {
     inputFileRef.current.click();
+    // ***
     inputFileRef.current.value = "";
   };
 
@@ -28,19 +29,21 @@ function FileUploader(props) {
     e.stopPropagation();
   };
 
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const droppedFiles = Array.from(e.dataTransfer.files);
-    const fileData = droppedFiles.map((file) => {
-      return { name: file.name };
-    });
 
-    setFiles((prev) => [...prev, ...fileData]);
-  };
+
+  // const handleDrop = (e) => {
+  //   e.preventDefault();
+  //   const droppedFiles = Array.from(e.dataTransfer.files);
+  //   const fileData = droppedFiles.map((file) => {
+  //     return { name: file.name };
+  //   });
+
+  //   setFiles((prev) => [...prev, ...fileData]);
+  // };
 
   return (
     <div>
-      <h2>Hello bruv</h2>
+      <h2>Hello !</h2>
       <input
         type="file"
         hidden
@@ -52,7 +55,7 @@ function FileUploader(props) {
       <div
         onDragEnter={handleDefaults}
         onDragOver={handleDefaults}
-        onDrop={handleDrop}
+        onDrop={(e)=>e.preventDefault()}
         style={{ padding: "100px", border: "1px dashed", margin: "10px" }}
       >
         Drop Your files here
