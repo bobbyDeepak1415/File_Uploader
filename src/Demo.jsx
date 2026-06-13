4ecf8ada6363a30875c9fcebdc12847c1e72be0f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-13 18:36:44 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Demo = () => {
-  return <div></div>;
+  const [files, setFiles] = useState([]);
+  const inputFileRef = useRef(null);
+
+  const handleSelect = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+
+    const fileData = selectedFiles.map((file) => {
+      return { name: file.name };
+    });
+
+    setFiles((prev) => [...prev, ...fileData]);
+  };
+
+  const handleClick = () => {
+    inputFileRef.current.click();
+    // ***
+    inputFileRef.current.value = "";
+  };
+
+  const handleDefaults = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const droppedFiles = Array.from(e.dataTransfer.files);
+    const fileData = droppedFiles.map((file) => {
+      return { name: file.name };
+    });
+
+    setFiles((prev) => [...prev, ...fileData]);
+  };
+
+  return (
+    <div>
+      <h2>Hello !</h2>
+      <input
+        type="file"
+        hidden
+        ref={inputFileRef}
+        multiple
+        onChange={handleSelect}
+      />
+      <button onClick={handleClick}>Browse Files</button>
+      <div
+        onDragEnter={handleDefaults}
+        onDragOver={handleDefaults}
+        onDrop={handleDrop}
+        style={{ padding: "100px", border: "1px dashed", margin: "10px" }}
+      >
+        Drop Your files here
+      </div>
+      <div>
+        {files.map((file, index) => {
+          return <li key={index}>{file.name}</li>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
