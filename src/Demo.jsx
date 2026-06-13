3c3abd1800ef5793784986d448a98831b353de77
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-13 13:42:11 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,25 +1,65 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Demo = () => {
+  const [files, setFiles] = useState([]);
   const inputFileRef = useRef(null);
 
   const handleSelect = (e) => {
-    const selectedFiles=Array.from(e.target.files)
+    const selectedFiles = Array.from(e.target.files);
 
-    const fileData=selectedFiles.map((file)=>)
+    const fileData = selectedFiles.map((file) => {
+      return { name: file.name };
+    });
+
+    setFiles((prev) => [...prev, ...fileData]);
   };
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    inputFileRef.current.click();
+    inputFileRef.current.value = "";
+  };
+
+  const handleDefaults=(e)=>{
+    e.preventDefault()
+  }
+
 
   return (
     <div>
       <input
-        value={inputFileRef}
+        type="file"
+        ref={inputFileRef}
         onChange={handleSelect}
         hidden
         multiple
       />
       <button onClick={handleClick}>Browse Files</button>
+
+      <div
+        style={{
+          display: "flex",
+          height: "50vh",
+          width: "90vw",
+          alignItems: "center",
+          justifyContent: "center",
+          border: "1px dashed",
+          margin: "auto",
+          marginTop: "40px",
+          
+        }}
+        onDragEnter={handleDefaults}
+        onDragOver={handleDefaults}
+        onDrop={}
+
+      >
+        Drop Your Files Here
+      </div>
+
+      <div>
+        {files.map((file, index) => {
+          return <li key={index}>{file.name}</li>;
+        })}
+      </div>
     </div>
   );
 };
