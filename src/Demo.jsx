9225cc8b20174e7475ba94a1a8f9dbb189ea82f3
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-05 15:38:23 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Demo = () => {
-  return <div>
-    <h2>FileUpLoader</h2>
-  </div>;
+  const inputFileRef = useRef();
+
+  const handleClick = () => {
+    inputFileRef.current.click();
+    inputFileRef.current.value = "";
+  };
+
+  return (
+    <div>
+      <h2>
+        FileUpLoader
+        <input ref={inputFileRef} type="file" hidden multiple />
+        <button onClick={handleClick}>Click</button>
+      </h2>
+    </div>
+  );
 };
 
 export default Demo;
