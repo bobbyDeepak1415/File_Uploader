7729726c65d848c5bb8caf374532696b7abd6b9f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-05 16:04:10 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,52 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from 'react'
 
 const Demo = () => {
-  const inputFileRef = useRef(null);
-
-  const [selectedFiles, setSelectedFiles] = useState(() => {
-    const files = localStorage.getItem("selectedFiles");
-    return files ? JSON.parse(files) : [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem("selectedFiles", JSON.stringify(selectedFiles));
-  }, [selectedFiles]);
-
-  const handleClick = () => {
-    inputFileRef.current.click();
-  };
-
-  const handleChange = (e) => {
-    const fileData = Array.from(e.target.files);
-
-    const fileNames = fileData.map((file) => {
-      return {
-        name: file.name,
-      };
-    });
-
-    setSelectedFiles((prev) => [...prev, ...fileNames]);
-  };
-
   return (
     <div>
-      <input
-        onChange={handleChange}
-        ref={inputFileRef}
-        type="file"
-        hidden
-        multiple
-      />
-      <button onClick={handleClick}>Add Files</button>
-      <div>
-        <ul>
-          {selectedFiles.map((file) => {
-            return <li key={file.id}>{file.name}</li>;
-          })}
-        </ul>
-      </div>
+      Demo
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
