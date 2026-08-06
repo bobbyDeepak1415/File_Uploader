d216a78818eff26ba3c39c19b14e967e2d56bd50
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-07 02:31:53 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,14 +3,14 @@ import React, { useEffect, useRef, useState } from "react";
 const Demo = () => {
   const inputFileRef = useRef(null);
 
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState(() => {
+    const files = localStorage.getItem("files");
+    return files ? JSON.parse(files) : [];
+  });
 
-  // const [files, setFiles] = useState(()=>{
-  //   const files=localStorage.getItem("files")
-  //   return files ? JSON.parse(files) : []
-  // });
-
-  // useEffect()
+  useEffect(() => {
+    localStorage.setItem("files", JSON.stringify(files));
+  }, [files]);
 
   const handleSelect = (e) => {
     e.preventDefault();
