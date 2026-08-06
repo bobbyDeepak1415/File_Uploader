726f8c14331a49c99f678953647c89080a4aa768
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-07 02:03:02 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -5,12 +5,25 @@ const Demo = () => {
 
   const inputFileRef = useRef(null);
 
-  const handleSelect = () => {};
+  const handleSelect = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+    const fileNames = selectedFiles.map((file) => {
+      return {
+        name: file.name,
+      };
+    });
+
+    setFiles((prev) => [...prev, ...fileNames]);
+  };
+  const handleClick = () => {
+    inputFileRef.current.click();
+    inputFileRef.current.value = "";
+  };
 
   return (
     <div>
       <input ref={inputFileRef} multiple hidden onChange={handleSelect} />
-      <button>Browse files</button>
+      <button onClick={handleClick}>Browse files</button>
       <div>
         <h2>Your files here:</h2>
       </div>
