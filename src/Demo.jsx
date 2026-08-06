2e98c13362d60490455fb13977a89ea1ac15547f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-07 02:07:54 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Demo = () => {
-  const [files, setFiles] = useState([]);
-
   const inputFileRef = useRef(null);
+
+  const [files,setFiles]=useState([])
+
+  // const [files, setFiles] = useState(() => {
+  //   const files = localStorage.getItem("files");
+  //   return files ? JSON.parse(files) : [];
+  // });
+
+
+  // useEffect(() => {
+  //   localStorage.setItem("files", JSON.stringify(files));
+  // }, [files]);
 
   const handleSelect = (e) => {
     const selectedFiles = Array.from(e.target.files);
@@ -22,13 +32,22 @@ const Demo = () => {
 
   return (
     <div>
-      <input ref={inputFileRef} multiple hidden onChange={handleSelect} />
+      <input
+        type="file"
+        ref={inputFileRef}
+        multiple
+        hidden
+        onChange={handleSelect}
+      />
       <button onClick={handleClick}>Browse files</button>
       <div>
         <h2>Your files here:</h2>
       </div>
-
-      {files}
+      <ul>
+        {files.map((file, index) => {
+          return <li key={index}>{file.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
