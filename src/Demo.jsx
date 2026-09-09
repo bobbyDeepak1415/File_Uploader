ae8b3d064eba3efefcced5b6580dc701c6fb868f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-10 00:18:22 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -4,7 +4,13 @@ const Demo = () => {
 
   const inputFileRef=useRef(null)
 
-  const [selectedFiles,setSelectedFiles]=useState([])
+  const [files,setFiles]=useState([])
+  // const [files,setFiles]=useState(()=>{
+  //   const files=localStorage.getItem("files")
+  //   return files? JSON.parse(files) :[]
+  // })
+
+
 
   const handleClick=()=>{
     inputFileRef.current.click()
@@ -12,9 +18,28 @@ const Demo = () => {
 
   }
 
+
+  const handleSelect=(e)=>{
+    const selectedFiles=Array.from(e.target.files)
+    const fileNames=selectedFiles.map((file)=>{
+      return {name:file.name}
+    })
+    setFiles((prev)=>[...prev,...fileNames])
+  }
+
   return <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
-    <input ref={inputFileRef} type="file" hidden onChange={(e)=>setSelectedFiles(e.target.files)}/>
+    <input multiple ref={inputFileRef} type="file" hidden onChange={handleSelect}/>
     <button onClick={handleClick}>Browse Files</button>
+
+<div>
+  <h2>Your files here:</h2>
+  <ul>
+    {files.map((file)=>{
+      return <li>{file.name}</li>
+    })}
+  </ul>
+</div>
+
   </div>;
 };
 
