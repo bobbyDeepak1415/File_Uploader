aefb7b598ff54b4408f7facb047e46c3f77e7ab2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-10 00:20:27 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,46 +1,11 @@
-import React, { useRef, useState } from "react";
+import React from 'react'
 
 const Demo = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
 
-  const inputFileRef=useRef(null)
-
-  const [files,setFiles]=useState([])
-  // const [files,setFiles]=useState(()=>{
-  //   const files=localStorage.getItem("files")
-  //   return files? JSON.parse(files) :[]
-  // })
-
-
-
-  const handleClick=()=>{
-    inputFileRef.current.click()
-    inputFileRef.current.value=""
-
-  }
-
-
-  const handleSelect=(e)=>{
-    const selectedFiles=Array.from(e.target.files)
-    const fileNames=selectedFiles.map((file)=>{
-      return {name:file.name}
-    })
-    setFiles((prev)=>[...prev,...fileNames])
-  }
-
-  return <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
-    <input multiple ref={inputFileRef} type="file" hidden onChange={handleSelect}/>
-    <button onClick={handleClick}>Browse Files</button>
-
-<div>
-  <h2>Your files here:</h2>
-  <ul>
-    {files.map((file)=>{
-      return <li>{file.name}</li>
-    })}
-  </ul>
-</div>
-
-  </div>;
-};
-
-export default Demo;
+export default Demo
